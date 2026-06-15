72d65bf26a57e6c39c7b6b1f68ddf918e5979765
MarkusErkmann <markus.erkmann@voco.ee>
2026-06-15 23:54:10 +0300

    Create connection to database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 require("dotenv").config();
+const PORT = process.env.PORT;
+
 
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT;
 
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+})
+
+sequelize.authenticate()
+.then(() => {
+    console.log("sql init success")
+})
+.catch((err) => {
+    console.log("error", err)
+})
 
 app.get("/", (req, res) => {
   res.send("ok");
