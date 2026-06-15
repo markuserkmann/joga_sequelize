8acd6bf86000c3e364b1f44f237d5d137f497787
MarkusErkmann <markus.erkmann@voco.ee>
2026-06-15 23:40:37 +0300

    Added dotenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-const express = require("express");
+require("dotenv").config();
 
+const express = require("express");
 const app = express();
+
+const PORT = process.env.PORT;
+
 
 app.get("/", (req, res) => {
   res.send("ok");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });
